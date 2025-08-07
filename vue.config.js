05de9ec336679a25034e4d2f89dee6458602beb0
Nahla Mohamed <nahlamohamed2372900@gmail.com>
2025-08-08 00:38:28 +0300

    solving

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
-      args[0].favicon = "/public/favicon.ico";
-      return args;
-    });
-  },
 });
