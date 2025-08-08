a5c71d304a7514384cc53d5b102ce40f39c28994
Nahla Mohamed <nahlamohamed2372900@gmail.com>
2025-08-08 04:51:29 +0300

    icon-update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    iconPaths: {
+      favicon32: "favlogo.png",
+      favicon16: "favlogo.png",
+      appleTouchIcon: "favlogo.png",
+      maskIcon: "favlogo.png",
+      msTileImage: "favlogo.png",
+    },
+  },
 });
